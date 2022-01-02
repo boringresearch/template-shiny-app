9ecfecf1ebfc70e7d7ae67d73321a4cedf50b4d7
xihajun <mw18386@my.bristol.ac.uk>
2022-01-02 18:23:25 +0000

    rm useless code

--- server.R
+++ server.R
@@ -2,36 +2,9 @@ library(shiny)
 library(shinyWidgets)
 library(shinydashboard)
 library(shinydashboardPlus)
-# loading ->  %>% withSpinner(color="blue")
 library(shinycssloaders)
-library(dplyr) 
-library(plotly)
-library(DT)
-library(magick)
-library(networkD3)
-library(rintrojs)
-library(dplyr)
-library(ggplot2)
-library(pheatmap)
-library("gplots")
-library(RColorBrewer)
-library(ComplexHeatmap)
-library(circlize)
-library(grid)
-library(VennDiagram)
-library(UpSetR)
-library(ggplotify)
-library(biomaRt)
-library(tidyverse)
-library(processx)
-library(org.Hs.eg.db)
-library(formattable)
-library(GeneNetworkBuilder)
-library(simpIntLists)
-library(networkD3)
-library(fgsea)
 
-# splitvec for split input with . ----
+# function ----
 splitvec <- function(vector, split, select, merge = "_"){
   processed <- sapply(vector, function(x){
     separated <- unlist(strsplit(x, split = split))[select]
@@ -46,20 +19,14 @@ splitvec <- function(vector, split, select, merge = "_"){
 
 server <- shinyServer(function(input, output, session) {
   
-  # init ----
+  # init eg.change some variable----
   v <- reactiveValues(upload = 0)
   observeEvent(input$submit, {
     v$upload <- 1
   })
-  observeEvent(input$load_default_data, {
-    v$upload <- 'quickload'
-  })
-  # https://stackoverflow.com/questions/42460509/r-shiny-passing-youtube-links-to-iframe
-  output$video <- renderUI({
-    #tags$iframe(src = "youtube-url",width = 600, height = 400)
-  })
   # I. load data ----
-  ######result######
+  
+  ######read ddata######
   result <- reactive({
     #result <- read.csv("data/xxx.csv")
     return(result)
@@ -67,14 +34,11 @@ server <- shinyServer(function(input, output, session) {
   
   # p1. show a button once uploaded a file ----
   output$uiaction <- renderUI({
-    
     column(6, offset = 0, actionButton("uiaction", "Run"))
-    
   })
   
   # p1. load default rna-seq data ----
   df <- eventReactive(input$load_default_data, {
-   
     return(df)
   })
   
@@ -82,16 +46,12 @@ server <- shinyServer(function(input, output, session) {
   # p1. submit gene symbol with ID and genename ----
 
   datapasted <- eventReactive(input$submit, {
-    
-    
     return(datapasted)
-    
   })
   
   
-  # counter ----
+  # visitor counts ----
   output$counter <- renderValueBox({
-
     if (!file.exists("data/counter.Rdata"))
       counter <- 0
     else
